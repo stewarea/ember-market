e139d52af5efd6d6153686612ceebed50134a5b2
Angela Smith and evan stewart <evan@>
2016-11-02 10:07:27 -0700

    create computed header

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  heading: Ember.computed('product.name', 'product.price', function() {
+    return this.get('product.name') + ': $' + this.get('product.price');
+  })
 });
--- app/routes/category.js
+++ app/routes/category.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return Ember.RSVP.hash({
+      categories: this.store.findRecord('category', params.category_id);,
+      products: this.store.findAll('product')
+    });
+  }
 });
