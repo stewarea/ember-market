eb71dfea59e5dd663edef42f95223c35c5e991c5
Angela Smith and evan stewart <evan@>
2016-11-02 14:52:23 -0700

    add shopping cart service.

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -1,8 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // heading: Ember.computed('category.products.name', 'category.products.price', function() {
+  //   return this.get('category.products.name') + ': $' + this.get('category.products.price');
+  // }),
+  shoppingCart: Ember.inject.service(),
+  actions: {
+    addToCart(product) {
+      this.get('shoppingCart').add(product);
+    }
+  }
 
-  heading: Ember.computed('product.name', 'product.price', function() {
-    return this.get('product.name') + ': $' + this.get('product.price');
-  })
 });
