5badcc32c4c0113e8332b7a6e0c2d51bff5f4b23
Angela Smith and evan stewart <evan@>
2016-11-03 08:46:42 -0700

    add sorted computed property to the product tile.

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -4,6 +4,9 @@ export default Ember.Component.extend({
   // heading: Ember.computed('category.products.name', 'category.products.price', function() {
   //   return this.get('category.products.name') + ': $' + this.get('category.products.price');
   // }),
+  sortBy: ['price'],
+  sortedProducts: Ember.computed.sort('category.products', 'sortBy'),
+  
   cart: Ember.inject.service('shopping-cart'),
   actions: {
     addToCart(product) {
